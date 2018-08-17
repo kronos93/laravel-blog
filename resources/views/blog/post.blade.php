3a68a16d3e43b4519307ea61d19c528c94693de2
Samuel R <desconocido_-_93@hotmail.com>
2018-08-17 12:56:08 -0500

    Add pagination

--- resources/views/blog/post.blade.php
+++ resources/views/blog/post.blade.php
@@ -2,12 +2,26 @@
 
 @section('content')
     <div class="container">
-        Listado de post
+        <h1>Listado de post</h1>
+        <div class="row">
 
-        @forelse($posts as $post)
-            <h1>{{$post->name}}</h1>
-        @empty
-            <p>Sin publicaciones para mostrar :(</p>
-        @endforelse
+            @forelse($posts as $post)
+            <div class="col-sm-4">
+                <div class="card">
+                    @if($post->file)
+                    <img class="card-img-top" src="{{ $post->file }}" alt="Card image cap">
+                    @endif
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $post->name }}</h5>
+                        <p class="card-text">{{ $post->excerpt }}...</p>
+                        <a href="#" class="btn btn-primary">Leer más</a>
+                    </div>
+                </div>
+            </div>
+            @empty
+                <p>Sin publicaciones para mostrar :(</p>
+            @endforelse
+        </div>
+        {{ $posts->links() }}
     </div>
 @endsection
